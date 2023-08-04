17d9afe093dca885bcd9e5672857f1133cd31391
Everton De Grande <evertondg@hotmail.com.br>
2023-08-03 23:30:51 -0300

    feat: Commit: Estrutura da aplicação - Cores & fonte
    Commit: Estrutura da aplicação - Cores & fonte

--- react/aulas/02-ignite-timer/src/App.tsx
+++ react/aulas/02-ignite-timer/src/App.tsx
@@ -1,15 +1,15 @@
 import { Button } from './components/Button';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary' />
-      <Button variant='secondary' />
-      <Button variant='success' />
-      <Button variant='warning' />
-      <Button variant='danger' />
-      <Button variant='neutral' />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
